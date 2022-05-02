71aa7a5d83e5f170142bf10315ee7f0b3a7237b3
YHhaoareyou <mlapenang1998@gmail.com>
2022-05-02 15:25:33 +0900

    Add build settings

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <img alt="Vue logo" :src="logo" />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
 </template>
 
 <script>
+import logo from './assets/logo.png'
+import parsePublicAssetsPath from './utils/parse-public-assets-path'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data: function () {
+    return {
+      logo: parsePublicAssetsPath(logo)
+    }
   }
 }
 </script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,53 @@
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dynamicImport from 'vite-plugin-dynamic-import'
+const path = require("path");
+const { parsed } = require("dotenv").config({
+  path: path.resolve(__dirname, "./src/.env"),
+});
 
-export default {
-  rollupOptions: {
-    input: 'src/main.ts',
-    format: 'system',
-    preserveEntrySignatures: true
-  },
-  base: 'http://localhost:3000',
-  plugins: [vue({
-    template: {
-      transformAssetUrls: {
-        base: '/src'
-      }
-    }
-  })],
-}
+export default defineConfig(({ mode }) => {
+  const publicAssetsBaseUrl =
+    mode === "production"
+      ? parsed.VITE_MF_VUE_PROD_DOMAIN + "/"
+      : "http://localhost:3000/";
+
+  return {
+    root: "./src",
+    base: publicAssetsBaseUrl,
+    rollupOptions: {
+      input: "vite-single-spa-vue.ts",
+      format: "system",
+      preserveEntrySignatures: true,
+    },
+    build: {
+      outDir: "../dist",
+      emptyOutDir: true,
+      cssCodeSplit: false,
+      manifest: true,
+      rollupOptions: {
+        input: "./src/vite-single-spa-vue.ts",
+        preserveEntrySignatures: true,
+        output: {
+          entryFileNames: "[name].js",
+          assetFileNames: "assets/[name].[ext]",
+        },
+      },
+    },
+    resolve: {
+      fullySpecified: false,
+      modules: ["node_modules"],
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
+    },
+    plugins: [
+      vue({
+        template: {
+          transformAssetUrls: {
+            base: '/src'
+          }
+        }
+      }),
+      dynamicImport(),
+    ],
+  };
+});
